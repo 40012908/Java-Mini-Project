96f95cd1e55d7f3659b948e397951cdafa8e719c
99002522 <dhanashree.vinherkar@ltts.com>
2020-11-09 17:22:36 +0530

    Added regular, prime and gold subclasses

---
+++ Gold.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Gold extends Validation {
+	public void goldMember() {
+		details();
+		System.out.println("You have been successfully registered for Gold Membership");
+		System.out.println("----------------------Registration Details--------------------------");
+		DateInfo date = new DateInfo();
+		date.getCurrentDate();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, 1);
+		Date nextYear = cal.getTime();
+		System.out.println("Expiration Date is " + " " + nextYear);
+	}
+
+}
